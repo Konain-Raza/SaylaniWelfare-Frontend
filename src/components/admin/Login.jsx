6787df3b05b2fccf69b0655268382c8cd9c8f129
Konain <konainraza105@gmail.com>
2025-01-29 03:38:09 +0500

    added testing credentials info on login page

--- src/components/admin/Login.jsx
+++ src/components/admin/Login.jsx
@@ -3,6 +3,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import api from "../../axios";
+import InfoAlert from "../SectionMessage";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -30,8 +31,9 @@ const Login = () => {
   };
 
   return (
-    <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+    <section className="bg-gray-50 min-h-screen flex flex-col items-center justify-center">
       <ToastContainer />
+      <InfoAlert/>
       <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
         <div className="flex justify-center mb-6">
           <img
