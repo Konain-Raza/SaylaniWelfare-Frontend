a934f70a27a85be53e2abfa103bb083e2116ed56
Konain <konainraza105@gmail.com>
2025-01-29 03:15:06 +0500

    added charts, delete user, update ui, changed menu names, axios instance config changes

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
   import axios from 'axios';
 
   const apiClient = axios.create({
-    baseURL:"http://localhost:3000",
-    // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3400',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3400',
     timeout: 10000,
     headers: {
       'Content-Type': 'application/json',
